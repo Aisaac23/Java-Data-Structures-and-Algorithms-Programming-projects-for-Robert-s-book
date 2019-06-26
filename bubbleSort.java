e6a4521a67ae01f06e79f04539a8e21edf077836
Aisaac23 <50410328+Aisaac23@users.noreply.github.com>
2019-06-26 14:51:59 -0500

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -4,62 +4,59 @@
 ////////////////////////////////////////////////////////////////
 class ArrayBub
    {
-   private long[] a;                 // ref to array a
-   private int nElems;               // number of data items
-//--------------------------------------------------------------
-   public ArrayBub(int max)          // constructor
-      {
-      a = new long[max];                 // create the array
-      nElems = 0;                        // no items yet
-      }
-//--------------------------------------------------------------
-   public void insert(long value)    // put element into array
-      {
-      a[nElems] = value;             // insert it
-      nElems++;                      // increment size
-      }
-//--------------------------------------------------------------
-   public void display()             // displays array contents
-      {
+   private long[] a;                
+   private int nElems;              
+   
+   public ArrayBub(int max)         
+   {
+      a = new long[max];                
+      nElems = 0;                       
+   }
+   
+   public void insert(long value)    
+   {
+      a[nElems] = value;            
+      nElems++;                     
+   }
+
+   public void display()             
+   {
       for(int j=0; j<nElems; j++)       // for each element,
          System.out.print(a[j] + " ");  // display it
       System.out.println("");
-      }
-//--------------------------------------------------------------
+   }
+   
    public void bubbleSort()
-      {
+   {
       int out, in;
 
       for(out=nElems-1; out>1; out--)   // outer loop (backward)
          for(in=0; in<out; in++)        // inner loop (forward)
             if( a[in] > a[in+1] )       // out of order?
                swap(in, in+1);          // swap them
-      }  // end bubbleSort()
+   } 
 
    
 
 //************************* Programming Projects: 3.1 *****************   
    public void bubbleSortBidirectional()
-      {
+   {
       int out, in, outBackw, nst = 0;
 
-      for(out=nElems-1; out>1; out--){   // outer loop (backward)
+      for(out=nElems-1; out>1; out--)
+      {
           //System.out.print(out+".. ");this.display();
-         for(in=nst; in<out; in++){
-             //System.out.print(in + "........... ");this.display();// inner loop (forward)
+         for(in=nst; in<out; in++)
+             //System.out.print(in + "........... ");this.display();
             if( a[in] > a[in+1] )       // out of order?
                swap(in, in+1);
-         }
-         for (outBackw = in-1; outBackw >nst; outBackw--){
+         for (outBackw = in-1; outBackw >nst; outBackw--)
              //System.out.print(in+"********************* ");this.display();
              if( a[outBackw] < a[in-1] )       // out of order?
-               swap(outBackw, outBackw-1);
-             
-            
-         }
+               swap(outBackw, outBackw-1)
           nst++;
-      }// swap them
-      }  // end bubbleSort()
+      }
+   } 
 //************************************************************************
 
 //************************* Programming Projects: 3.4 *****************   
@@ -92,9 +89,9 @@ public void oddEvenSort()
       a[one] = a[two];
       a[two] = temp;
       }
-//--------------------------------------------------------------
+
    }  // end class ArrayBub
-////////////////////////////////////////////////////////////////
+
 class BubbleSortApp
    {
    public static void main(String[] args)
