c033ab71a98f218823a13bb2bd6004eb67e7ad1f
Aisaac23 <50410328+Aisaac23@users.noreply.github.com>
2019-07-05 13:30:58 -0500

    Update CircularList.java

--- CircularList.java
+++ CircularList.java
@@ -5,73 +5,10 @@
  */
 package circularlist;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-class Link
-{
-    public int iData;
-    public double dData;
-    public Link next;
-
-    public Link(int iData, double dData) {
-        this.iData = iData;
-        this.dData = dData;
-    }
-    
-    public void displayLink()
-    {
-        System.out.print("{" + this.dData + ", " + this.iData + "}");
-    }
-}
-
-
-class LinkList
-{
-    private Link first;
-    public LinkList()
-    {
-        first = null;
-    }
-    
-    public boolean isEmpty()
-    {
-        return (first == null);
-    }
-    
-    public void insert(int id, double dd)
-    {
-        Link newLink = new Link(id, dd);
-        if(first == null)
-        {
-            newLink.next = first;
-            first = newLink;
-        }
-        else
-        {
-            Link temp = first;
-            while(temp.next != null)
-                temp = temp.next;
-            temp.next = newLink;
-            newLink.next = null;
-        }
-    }
-    public void displayList()
-    {
-        Link temp = first;
-        while(temp != null)
-        {
-            temp.displayLink();
-            temp = temp.next;
-        }
-        System.out.println("");
-    }
-}
-
 public class CircularList {
 
     
-    private Link current;
+    private CustomLink current;
     public CircularList()
     {
         this.current = null;
@@ -81,141 +18,80 @@ public class CircularList {
     {
         return this.current == null;
     }
-    
-    public LinkList JosephusProblem(int cOff)
-    {
-        int killer = cOff;
-        LinkList eliminated = new LinkList();
-        current = current.next;//go to the first element
-        
-        while(current.next != current)
-        {
-            Link temp = this.deleteSpin(killer);
-            eliminated.insert(temp.iData, temp.dData);
-            
-        }
-        return eliminated;
-    }
-    
-    /****************** Programming projects: 5.3 ***********************/    
-    public Link deleteSpin(int cOff)
-    {
-        if(current.next == current)
-            return null;
-        else
-        {
-            Link prev = current;
-            for (int a = 0; a < cOff; a++) {
-                prev = current;
-                current = current.next;
-            }
-            Link temp = current;
-            prev.next = current.next;
-           current = prev.next;
-            return temp;
-        }
-    }
-    /*********************************************************************/
-    
+   
     /****************** Programming projects: 5.3 ***********************/
-    public void insert(int id, double dd)
+    public void insert(long id, double dd)
     {
-        if(current == null)
+        CustomLink item = new CustomLink(id, dd);
+        if( this.isEmpty() )
         {
-            current = new Link(id, dd);
-            current.next = current;
+            current = item;
+            current.setNext(current);
         }
         else
         {
-            Link item = new Link(id, dd);
-            item.next = current.next;
-            current.next = item;
+            item.setNext(current.getNext());
+            current.setNext(item);
             current = item;
         }
     }
-    /*********************************************************************/
+    
+    public void step(CustomLink move)
+    {
+        move = (CustomLink) move.getNext();
+    }
+    
+    public long find( double dd )
+    {
+        long steps = -1;
+        if( !this.isEmpty() )
+        {
+            CustomLink toMove = current;
+            if( toMove.getdData() == dd )
+                return 0;
+            else
+            {
+                long ref = toMove.getiData();
+                toMove = (CustomLink) toMove.getNext();//this.step(toMove);
+                steps++;
+                
+                while( toMove.getdData()!=dd && toMove.getiData()!=ref )
+                {
+                    steps++;
+                    toMove = (CustomLink) toMove.getNext();//this.step(toMove);
+                }
+                
+                return ( toMove.getdData()==dd ) ? steps : -1;
+            }
+        }
+        return steps;
+    }
     
     public void display()
     {
         if(!this.isEmpty())
         {
-            int ref = current.iData;
-            while(current.next.iData != ref)
+            CustomLink temp = current;
+            long ref = temp.getiData();
+            temp = (CustomLink) temp.getNext();
+            while(temp.getiData() != ref)
             {
-                System.out.print("["+current.dData+"]->");
-                current = step();
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(CircularList.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                temp.displayLink();
+                temp = (CustomLink) temp.getNext();
             }
-            System.out.print("["+current.dData+"]->");
+            temp.displayLink();
         }
         else
             System.out.println("Empty list.");
     }
     
-    public Link delete(double dd)
-    {
-        if(current.next == current)
-        {
-            Link temp = current;
-            current = null;
-            return temp;
-        }
-
-        Link prev = current;
-        current = step();
-        int ref = current.iData;
-        
-        while(true)
-        {
-            if(current.iData == dd)
-            {
-                Link temp = current;
-                prev.next = current.next;
-                current = prev.next;
-                return temp;
-            }
-            else
-            {
-                prev = current;
-                current = step();
-            }
-            if(current.iData == ref)
-                break;
-        }
-        return null;
-    }
-    public Link step()
-    {
-        return current.next;
-    }
-    
     public static void main(String[] args) {
-        CircularList obj = new CircularList();
-        for (int i = 0; i < 10; i++) {
-            obj.insert(i, i*Math.PI);
-        }
-        obj.display();
-        System.out.println("");
-        for (int i = 0; i < 9; i++)
-           obj.delete(i).displayLink();
-        obj.display();
-        obj.delete(9);
-        System.out.println("");
-        obj.display();
+        int maxSize = 100;
+        CircularList Clist = new CircularList(); 
+        for (int i = 0; i < maxSize/2; i++)
+            Clist.insert(i, (long)(Math.random()*maxSize));
+        Clist.display();
         
-        CircularList listC = new CircularList();
-        for (int i = 1; i < 4; i++) {
-            listC.insert(i, i*100);
-        }
-        //listC.display();
-        LinkList elim;
-        elim = listC.JosephusProblem(3);
-        elim.displayList();
-        listC.display();
     }
     
 }
