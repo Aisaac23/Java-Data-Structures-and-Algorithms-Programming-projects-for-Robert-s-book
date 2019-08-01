e7d8cf89d3e92caa1d2a0de4d0c357cf65a5db72
Aisaac23 <50410328+Aisaac23@users.noreply.github.com>
2019-08-01 11:39:19 -0500

    Update bubbleSort.java

--- Chapter 3/bubbleSort.java
+++ Chapter 3/bubbleSort.java
@@ -1,7 +1,7 @@
-// bubbleSort.java
-// demonstrates bubble sort
-// to run this program: C>java BubbleSortApp
-////////////////////////////////////////////////////////////////
+//This is a solution or part of a solution proposed by: 
+//33ED4C419BD9178A7EDB80708518BF447F43F3980971CE6CFB7D86C1CBC75ACE8CA0417ABF1ADEFCE44826BD725CD1382DA0C0B3AE577DA326EC8817DB336E11
+///////////////////////////////////////////////////////////////
+
 class ArrayBub
    {
    private long[] a;                
