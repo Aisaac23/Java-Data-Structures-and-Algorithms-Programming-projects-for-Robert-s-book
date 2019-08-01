6b9128c852d931dfc4d8d11dd47eef661e1ac8c1
Aisaac23 <50410328+Aisaac23@users.noreply.github.com>
2019-08-01 11:54:53 -0500

    Update doublyLinked.java

--- Chapter 5/doublyLinked.java
+++ Chapter 5/doublyLinked.java
@@ -1,10 +1,11 @@
+//This is a solution or part of a solution proposed by: 
+//33ED4C419BD9178A7EDB80708518BF447F43F3980971CE6CFB7D86C1CBC75ACE8CA0417ABF1ADEFCE44826BD725CD1382DA0C0B3AE577DA326EC8817DB336E11
+///////////////////////////////////////////////////////////////
+
 package LinkedLists;
 
 import Links.*;
-// doublyLinked.java
-// demonstrates doubly-linked list
-// to run this program: C>java DoublyLinkedApp
-////////////////////////////////////////////////////////////////
+
 class DoublyLinkedList
 {
     private DoubleLink first;
