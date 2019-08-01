aa972efc12699678d968fe44634606b507295966
Aisaac23 <50410328+Aisaac23@users.noreply.github.com>
2019-08-01 11:47:28 -0500

    Update sortedList.java

--- Chapter 5/sortedList.java
+++ Chapter 5/sortedList.java
@@ -1,12 +1,10 @@
+//This is a solution or part of a solution proposed by: 
+//33ED4C419BD9178A7EDB80708518BF447F43F3980971CE6CFB7D86C1CBC75ACE8CA0417ABF1ADEFCE44826BD725CD1382DA0C0B3AE577DA326EC8817DB336E11
+///////////////////////////////////////////////////////////////
+
 package sortedList;
 
-// sortedList.java
-
 import Links.Link;
-
-// demonstrates sorted list
-// to run this program: C>java SortedListApp
-////////////////////////////////////////////////////////////////
 
 class SortedList
 {
