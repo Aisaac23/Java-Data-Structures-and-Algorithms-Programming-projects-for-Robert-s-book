4ba56119491e0039e2f52a9cdd98305292fbed1f
Aisaac23 <50410328+Aisaac23@users.noreply.github.com>
2019-08-01 11:36:33 -0500

    Update orderedArray.java

--- Chapter 2/orderedArray.java
+++ Chapter 2/orderedArray.java
@@ -1,9 +1,8 @@
 package orderedarray;
 
-// orderedArray.java
-// demonstrates ordered array class
-// to run this program: C>java OrderedApp
-////////////////////////////////////////////////////////////////
+//This is a solution or part of a solution proposed by: 
+//33ED4C419BD9178A7EDB80708518BF447F43F3980971CE6CFB7D86C1CBC75ACE8CA0417ABF1ADEFCE44826BD725CD1382DA0C0B3AE577DA326EC8817DB336E11
+///////////////////////////////////////////////////////////////
 class OrdArray
 {
       private long[] a;                 
