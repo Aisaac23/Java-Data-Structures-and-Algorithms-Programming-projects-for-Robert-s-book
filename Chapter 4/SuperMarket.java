265c0cf2e691875d3fdb37939d194ab135d3525e
Aisaac23 <50410328+Aisaac23@users.noreply.github.com>
2019-08-01 11:40:44 -0500

    Update SuperMarket.java

--- Chapter 4/SuperMarket.java
+++ Chapter 4/SuperMarket.java
@@ -1,8 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//This is a solution or part of a solution proposed by: 
+//33ED4C419BD9178A7EDB80708518BF447F43F3980971CE6CFB7D86C1CBC75ACE8CA0417ABF1ADEFCE44826BD725CD1382DA0C0B3AE577DA326EC8817DB336E11
+///////////////////////////////////////////////////////////////
 package Queue;
 
 import java.util.logging.Level;
